0a03f1da4451abecedbd12f0d1810d59034c50ab
CelestinFernandes <celestin.fernandes21@st.niituniversity.in>
2024-08-21 19:53:01 +0530

    Subtraction

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import Addition
+import Subtraction
 
 while True:
     print("Let's perform a calculation!")
@@ -20,6 +21,15 @@ while True:
         print(f'The result of addition of {user_input} is: {add_value} \n')
 
     # elif operation = 1, 2, 3
+
+    if operation == 2:
+        try:
+            x, y = map(int, input("Enter two numbers you want to subtract (space-separated): ").split())
+        except ValueError:
+            print("Wrong input, recheck input")
+            continue
+        sub_value = Subtraction.subtract(x,y)
+        print(f'The result of subtraction of {x} and {y} is: {sub_value} \n')
 
     elif ((operation == 4) or (operation not in [0, 1, 2, 3])):
         break
