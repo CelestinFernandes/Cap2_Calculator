53389a7bd0fac4343ff4c55c2eb489d187965724
dikshant sharma <dikshant.sharma21@st.niituniversity.in>
2024-08-21 19:28:42 +0530

    added division in calc.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 import Addition
+import Division
 
 while True:
     print("Let's perform a calculation!")
@@ -20,6 +21,17 @@ while True:
         print(f'The result of addition of {user_input} is: {add_value} \n')
 
     # elif operation = 1, 2, 3
+    if operation == 3:
+        try:
+            # user_input = list(map(int, input("Enter numbers you want to divide (space-separated): ").split()))
+            inp1 = float(input("Enter the first number: "))
+            inp2 = float(input("Enter the second number: "))
+            
+        except ValueError:
+            print("Wrong input, recheck input")
+            continue
+        div_value = Division.divide(inp1, inp2)
+        print(f'The result of division of {inp1}/{inp2} is: {div_value} \n')
 
     elif ((operation == 4) or (operation not in [0, 1, 2, 3])):
         break
